f15a0e7d449e78a6a1304fd6f814210e86b5c058
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2026-03-02 18:14:27 -0600

    feat: (DatabaseSeeder) :sparkles: Add an admin user to the seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+        ]);
 
         $this->call([
             ProveedorSeeder::class,
